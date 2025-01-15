4dc78b5858657f89cfbb76334fb9e4aa64fffafa
Jack Naylor <jacknaylor999@hotmail.com>
2025-01-15 11:58:33 +0700

    added local storage

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ const itemContainer = document.getElementById("container");
 const textInput  = document.getElementById("input");
 const reset = document.getElementById("reset");
 
+// Need to add load function to arguement
+document.addEventListener('DOMContentLoaded', loadTodos());
+
 addButton.addEventListener('click' , addNew);
 
 function addNew() {
@@ -24,55 +27,86 @@ function addNew() {
     removeButton.className ="add";
     removeButton.classList.add("removeRed");
 
-
     // Remove current clicked ToDo
-
     removeButton.addEventListener("click", () => {
         itemContainer.removeChild(newDiv);
+        removeTodos(newInput.value); // Remove from localStorage
         console.log("Removed a todo item");
     });
 
     // Add toDo to list
-    
     newDiv.appendChild(newInput);
     newDiv.appendChild(removeButton);
+
+    // Saves text for Todo
+    saveTodos(textVal);
     
     itemContainer.appendChild(newDiv);
     
     textInput.value = "";
 
-
-
+    // Removes all added Todos
     reset.addEventListener('click' , () =>{
-        // If the Div contains a button with remove element then remove all of these
-        console.log("reset");
-    
         if (document.body.contains(newDiv)){
-            console.log("There is a new DIV");
             itemContainer.removeChild(newDiv);
+            localStorage.clear('todos'); // Removes all todos from local storage
         }
-    
     });
+};
 
+// Saves/Removes/Loads ToDos from local storage
+
+function saveTodos(todoText) {
+    let todos =  JSON.parse(localStorage.getItem('todos')) || []; 
+    todos.push(todoText); // Add new toDo to text
+    localStorage.setItem('todos', JSON.stringify(todos)); // Save toDo list
 }
 
-// FIX
+function removeTodos(todoText){
+    let todos =  JSON.parse(localStorage.getItem('todos')) || []; 
+    todos = todos.filter(todo => todo !== todoText); // Filters out todos that have been added and removed 
+    localStorage.setItem('todos', JSON.stringify(todos)); // Save updated list
+}
 
-// reset.addEventListener('click' , () =>{
-//     // If the Div contains a button with remove element then remove all of these
-//     console.log("reset");
+// let todos =  JSON.parse(localStorage.getItem('todos')) || [];  Ensures that there will always be an array when there are no todos saved
 
-//     if (document.body.contains(newDiv)){
-//         console.log("There is a new DIV")
-//     }
+function loadTodos(todoText) {
+    let todos = JSON.parse(localStorage.getItem('todos')) || []; 
+    todos.forEach(todo => {
+        const newDiv = document.createElement("div");
+        newDiv.className ="todo";
+    
+        // Create new Input
+        const newInput = document.createElement("input");
+        newInput.className ="text";
+        newInput.value = todo;
+        newInput.disabled = true; // Make it read only
+    
+        // Create remove Button
+        const removeButton = document.createElement("button");
+        removeButton.className ="add";
+        removeButton.classList.add("removeRed");
+    
+        // Remove current clicked ToDo
+        removeButton.addEventListener("click", () => {
+        itemContainer.removeChild(newDiv);
+        removeTodos(newInput.value); // Remove from localStorage
+        console.log("Removed a todo item");
+        });
 
-// });
+        // Add toDo to list
+        newDiv.appendChild(newInput);
+        newDiv.appendChild(removeButton);
+    
+        // Saves text for Todo
+
+        itemContainer.appendChild(newDiv);
+    });
+};
 
 // Enter key used to add
-
 addEventListener('keydown', (e) => {
     if (e.key === 'Enter') {
         addNew();
     }
 });
-
